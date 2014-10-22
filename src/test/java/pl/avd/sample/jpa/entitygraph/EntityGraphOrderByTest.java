704a1d62159c668eed1aef76b4913af6fd50c62e
Piotr Ostrowski <yuppy@avd.pl>
2014-10-22 21:57:29 +0200

    added test without order

--- src/test/java/pl/avd/sample/jpa/entitygraph/EntityGraphOrderByTest.java
+++ src/test/java/pl/avd/sample/jpa/entitygraph/EntityGraphOrderByTest.java
@@ -22,7 +22,25 @@ public class EntityGraphOrderByTest {
     doGraphTest(oracleFactory.createEntityManager());
   }
   
+  @Test
+  public void hsqldbOrderByTest() {
+    doGraphOrderByTest(hsqldbFactory.createEntityManager());
+  }
+  
+  @Test
+  public void oracleOrderByTest() {
+    doGraphOrderByTest(oracleFactory.createEntityManager());
+  }
+  
   private void doGraphTest(EntityManager em) {
+    EntityGraph<?> graph = em.getEntityGraph(Car.GRAPH_FETCH_ALL);
+    
+    em.createQuery("select c from Car c", Car.class)
+        .setHint("javax.persistence.fetchgraph", graph)
+        .getResultList();
+  }
+  
+  private void doGraphOrderByTest(EntityManager em) {
     EntityGraph<?> graph = em.getEntityGraph(Car.GRAPH_FETCH_ALL);
     
     em.createQuery("select c from Car c order by c.manufacture.name", Car.class)
